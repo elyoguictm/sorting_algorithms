c57c9cb5e72cd8671cea06a8ac8d0bb763c401ba
elyoguictm <gabyferal@gmail.com>
2021-06-10 14:32:56 +0000

    add bubbleSort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - bubble sort algorithm
+ * @array: array
+ * @size: size of array
+ * Return: sorted array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+  size_t z, x;
+  int tmp;
+
+  for (x = 0; x < size; x++)
+    {
+      for (z = 0; z < size - 1; z++)
+	{
+	  if (array[z] > array[z + 1])
+	    {
+	      tmp = array[z];
+	      array[z] = array[z + 1];
+	      array[z + 1] = tmp;
+	      print_array(array, size);
+	    }
+	}
+    }
+}
