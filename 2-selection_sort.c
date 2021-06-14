3e7feecf328274010f3f3f74f732b5cce8000af1
elyoguictm <gabyferal@gmail.com>
2021-06-14 18:12:55 +0000

    rep

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,12 +1,11 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts an array
- *
- * @array: array of int
- * @size: size
+ * selection_sort - sorts an array of integers in ascending order
+ * using the Selection sort algorithm
+ * @array: array to be sorted
+ * @size: number of elements in @array
  */
-
 void selection_sort(int *array, size_t size)
 {
 	size_t z, x, tmp, tim;
