cb267e5cdd5f3ac8590993472b689cecd057de12
elyoguictm <gabyferal@gmail.com>
2021-06-16 21:01:46 +0000

    added insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,12 +1,35 @@
 #include "sort.h"
 
 /**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ *  in ascending order using the Insertion sort algorithm
  *
- *
- *
- *
+ * @list: head of list.
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *node, *n1;
 
+	if (list == NULL || *list == NULL)
+		return;
+	node = (*list)->next;
+	while (node)
+	{
+		n1 = node->next;
+		while (node->prev && node->prev->n > node->n)
+		{
+			node->prev->next = node->next;
+			if (node->next)
+				node->next->prev = node->prev;
+			node->next = node->prev;
+			node->prev = node->next->prev;
+			node->next->prev = node;
+			if (node->prev)
+				node->prev->next = node;
+			else
+				*list = node;
+			print_list(*list);
+		}
+		node = n1;
+	}
 }
